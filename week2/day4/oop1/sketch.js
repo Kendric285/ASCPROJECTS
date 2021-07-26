9f7ed84093c843b0cd0f81f6a45e4a9aad1deb72
Kendric285 <terrykendric@gmail.com>
2021-07-26 11:07:21 -0400

    oop

---
+++ week2/day4/oop1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(500, 500);
+    noStroke();
+}
+function draw() {
+    background(0)
+    fill(255,0,0);
+    ellipse(150,250,100,100)
+
+    fill(0,255,0);
+    ellipse(250,250,100,100)
+
+    fill(0,0,255);
+    ellipse(350,250,100,100)
+  
+}
