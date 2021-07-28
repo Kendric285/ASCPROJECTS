1f241047ee09aae5dca30246f9af1a2dc3145c64
Kendric285 <terrykendric@gmail.com>
2021-07-28 11:30:09 -0400

    everythinh

--- week2/day4/oop1/sketch.js
+++ week2/day4/oop1/sketch.js
@@ -1,16 +1,40 @@
+let ball1;
+
 function setup() {
     createCanvas(500, 500);
     noStroke();
+    ball1 = new Ball(150,250,255,0,0)
+    
 }
+
 function draw() {
     background(0)
-    fill(255,0,0);
-    ellipse(150,250,100,100)
 
-    fill(0,255,0);
-    ellipse(250,250,100,100)
+    
+    // fill(255,0,0);
+    // ellipse(150,250,100,100)
 
-    fill(0,0,255);
-    ellipse(350,250,100,100)
+    // fill(0,255,0);
+    // ellipse(250,250,100,100)
+
+    // fill(0,0,255);
+    // ellipse(350,250,100,100)
+    fill(ball1.rVal, ball1.gVal, ball1.bVal)
+    ellipse(ball1.xPos,ball1.yPos,100,100)
+    // console.log(ball1.add)
+
   
 }
+
+class Ball {
+    constructor(x, y, r, g, b){
+        this.xPos = x;
+        this.yPos = y;
+        this.rVal = r;
+        this.gVal = g;
+        this.bVal = b;
+
+        this.add = x + y;
+    }
+}
+
